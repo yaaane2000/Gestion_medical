eb9f6947cec78164976cd2a86b10f02646901183
yaaane2000 <161392294+yaaane2000@users.noreply.github.com>
2024-04-26 14:23:24 -0400

    Add files via upload

---
+++ ORACLE_DB/Medical.sql
@@ -0,0 +1,7 @@
+create table Patient (
+Num_patient number (20) primary key ,
+Nom_Patient varchar2(10) ,
+Prenom_Patient varchar2(10),
+Adresse varchar2(10)
+);
+--------------------------------------
---
+++ ORACLE_DB/adm.sql
@@ -0,0 +1,11 @@
+create table connexion (
+Utilisateur varchar2 (20),
+MOt_de_passe varchar2 (20)
+
+);
+insert into connexion(Utilisateur,MOt_de_passe)
+values ('docteur',1234) ;
+insert into connexion(Utilisateur,MOt_de_passe)
+values ('assistant(e)',5678) ;
+
+
